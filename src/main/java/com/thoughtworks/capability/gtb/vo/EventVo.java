452d7e2d7e627e9919e5b75f290f1b25965f8d3e
Suqin <suqin.zheng@thoughtworks.com>
2020-11-05 20:43:59 +0800

    [Suqin] feat:Jackson annotation to achieve and return JSON

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EventVo {
 
-  private String id;
-  private String name;
-  private EventType type;
-  private Date time;
-  private UserVo user;
+    private String id;
+    private String name;
+    private EventType type;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Date time;
+    @JsonUnwrapped
+    private UserVo user;
 }
